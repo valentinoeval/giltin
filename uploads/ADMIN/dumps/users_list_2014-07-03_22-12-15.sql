b800ddad54044b9307341407e6e7012f60597701
Valentin <valentinsomme@hotmail.fr>
2014-07-03 21:27:35 +0200

    maj

---
+++ uploads/ADMIN/dumps/users_list_2014-07-03_22-12-15.sql
@@ -0,0 +1,2 @@
+INSERT INTO giltin_users VALUES (2, 'KingVal', 'f7f8f973d33b664faf6585d020e59576d2b1427b', -2, 'Valentin Golliot', 'edb7a22208b398764dda9d38b1b25836.png', , 'valentinsomme@hotmail.fr', 'fr');
+INSERT INTO giltin_users VALUES (10, 'Zouini', '81aef62ed1e1b1bb2d9c253f2d25c285722fd201', -1, 'Yacine Zouini', '889b3344d2456cd4856f9f429db2918a.jpg', 5212a0ed880a0, 'jeanfrancois632@yahoo.fr', 'fr');
