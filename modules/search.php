038b3f566a914c56ecbb8639120002ddc3be6c97
Valentin Golliot <valentingolliot@MacBook-Pro-de-Valentin.local>
2014-08-20 04:26:23 +0200

    maj

--- modules/search.php
+++ modules/search.php
@@ -21,101 +21,105 @@
 
 		$_POST['name']=charSpec2code($_POST['name'], "convert");
 
-		//saffichage des options et informations
-		echo '<div class="conteneur_options">';
-
 		//vérification du type DD/MM/AAAA
-		if (preg_match("#^[0-9]{2}/[0-9]{2}/[0-9]{4}$#", $_POST['name'])) {
-			echo "Cas de recherche par <span>DD/MM/AAAA</span><br>";
+		if (preg_match("#^date:[0-9]{2}/[0-9]{2}/[0-9]{4}$#", $_POST['name'])) {
+			$html='Recherche par <span>DD/MM/AAAA</span><br>';
 			$_POST['name']=build_date_en($_POST['name']);
 			//récupération des opération bancaires
 			$req=$bddlog->query('SELECT * FROM giltin_comptes_'.$_SESSION['id'].' WHERE op_date="'.$_POST['name'].'"');
 			$req2=$bddlog->query('SELECT count(*) AS nb_op FROM giltin_comptes_'.$_SESSION['id'].' WHERE op_date="'.$_POST['name'].'"');
-			$code=true;
+			$flag=true;
 		}
 
 		//vérification du type MM/AAAA
-		elseif (preg_match("#^[0-9]{2}/[0-9]{4}$#", $_POST['name'])) {
-			list($mois, $annee)=explode('/', $_POST['name']);
-			echo 'Recherche des opérations du mois de '.dateInt2String($mois).' '.$annee;
-			$_POST['name']=date('Y-m', time($_POST['name']));
+		elseif (preg_match("#^date:[0-9]{2}/[0-9]{4}$#", $_POST['name'])) {
+			list($null, $date)=explode(':', $_POST['name']);die($date);
+			list($mois, $annee)=explode('/', $date);
+			$html='Recherche des opérations du mois de '.dateInt2String($mois).' '.$annee;
+			$_POST['name']=date('Y-m', time($date));
 			//récupération des opération bancaires
 			$req=$bddlog->query('SELECT * FROM giltin_comptes_'.$_SESSION['id'].' WHERE op_date like "'.$_POST['name'].'%"');
 			$req2=$bddlog->query('SELECT count(*) AS nb_op FROM giltin_comptes_'.$_SESSION['id'].' WHERE op_date like "'.$_POST['name'].'%"');
 		}
 
 		//vérification du type AAAA
-		elseif (preg_match("#^[0-9]{4}$#", $_POST['name'])) {
-			echo 'Recherche des opérations de l\'année <span>'.$_POST['name'].'</span><br />'; 
+		elseif (preg_match("#^date:[0-9]{4}$#", $_POST['name'])) {
+			list($null, $annee)=explode(':', $_POST['name']);
+			$html='Recherche des opérations de l\'année <span>'.$annee.'</span><br />'; 
 			//récupération des opération bancaires
-			$req=$bddlog->query('SELECT * FROM giltin_comptes_'.$_SESSION['id'].' WHERE op_date like "'.$_POST['name'].'%"');
+			$req=$bddlog->query('SELECT * FROM giltin_comptes_'.$_SESSION['id'].' WHERE op_date like "'.$annee.'%"');
 			$req2=$bddlog->query('SELECT count(*) AS nb_op FROM giltin_comptes_'.$_SESSION['id'].' WHERE op_date like "'.$_POST['name'].'%"');
-			$code=true;
+			$flag=true;
 		}
 
 		//vérification si la chaine est de type #x
-		elseif (preg_match("*^#[0-9]+$*", $_POST['name'])) {
-			echo "Cas de recherche par l'<span>#id</span> de l'op&eacute;ration bancaire<br>";
-			list($diese, $id)=explode("#", $_POST['name']);
+		elseif (preg_match("*^id:[0-9]+$*", $_POST['name'])) {
+			$html='Cas de recherche par l\'<span>#id</span> de l\'op&eacute;ration bancaire<br>';
+			list($null, $id)=explode(":", $_POST['name']);
 			$_POST['name']=$id;
 			//récupération des opération bancaires
 			$req=$bddlog->query('SELECT * FROM giltin_comptes_'.$_SESSION['id'].' WHERE id="'.$_POST['name'].'"');
 			$req2=$bddlog->query('SELECT count(*) AS nb_op FROM giltin_comptes_'.$_SESSION['id'].' WHERE id="'.$_POST['name'].'"');
-			$code=true;
+			$flag=true;
 		}
 
 		//vérification si la chaine est de type alphanumérique
 		elseif (preg_match("#^[a-zA-Z0-9][^ ]+$#", $_POST['name'])) {
-			echo "Cas de recherche par le <span>nom</span> de l'op&eacute;ration bancaire<br>";
+			$html='Cas de recherche par le <span>nom</span> de l\'op&eacute;ration bancaire<br>';
 			//récupération des opération bancaires
 			$req=$bddlog->query('SELECT * FROM giltin_comptes_'.$_SESSION['id'].' WHERE nom="'.$_POST['name'].'"');
 			$req2=$bddlog->query('SELECT count(*) AS nb_op FROM giltin_comptes_'.$_SESSION['id'].' WHERE nom="'.$_POST['name'].'"');
-			$code=true;
+			$flag=true;
 		}
 
 		//vérification si la chaine est de type numérique sans virgule
 		elseif (preg_match("#^[0-9]+€$#", $_POST['name'])) {
-			echo "Cas de recherche par le <span>montant</span> de l'op&eacute;ration bancaire<br>";
+			$html='Cas de recherche par le <span>montant</span> de l\'op&eacute;ration bancaire<br>';
 			//récupération des opération bancaires
 			$req=$bddlog->query('SELECT * FROM giltin_comptes_'.$_SESSION['id'].' WHERE montant="'.$_POST['name'].'"');
 			$req2=$bddlog->query('SELECT count(*) AS nb_op FROM giltin_comptes_'.$_SESSION['id'].' WHERE montant="'.$_POST['name'].'"');
-			$code=true;
+			$flag=true;
 		}
 
 		//vérification si la chaine est de type numérique avec virgule
 		elseif (preg_match("#^[0-9]+[.,][0-9]{1,2}€$#", $_POST['name'])) {
-			echo "Cas de recherche par le <span>montant</span> de l'op&eacute;ration bancaire<br>";
+			$html='Recherche par le <span>montant</span> de l\'op&eacute;ration bancaire<br>';
 			//récupération des opération bancaires
 			$req=$bddlog->query('SELECT * FROM giltin_comptes_'.$_SESSION['id'].' WHERE montant="'.$_POST['name'].'"');
 			$req2=$bddlog->query('SELECT count(*) AS nb_op FROM giltin_comptes_'.$_SESSION['id'].' WHERE montant="'.$_POST['name'].'"');
-			$code=true;
+			$flag=true;
 		}
 		
 		//comptage des opérations bancaires
 		$nb_op=0;
-		if (isset($code) and $code==true) {
+		if (isset($flag) and $flag==true) {
 			$datas2=$req2->fetch(PDO::FETCH_ASSOC);
 			$nb_op=$datas['nb_op'];
 		}
 
-		if (isset($code) and $code==true) {
-			//suite affichage des options et informations
-			echo 'Il y a <span>'.$nb_op.'</span> op&eacute;ration';if ($nb_op>1) echo 's'; echo ' bancaire';if ($nb_op>1) echo 's';
-			echo ' dans tous vos comptes pour votre recherche.</div><br>';
-
-			//affichage des opération trouvées dans les comptes
-			echo '<table cellspacing="0" cellpadding="0" id="tList">
-					<tr id="tHeader">
-						<td class="nom">Nom de l\'op&eacute;ration bancaire</td>
-						<td class="date">Date</td>
-						<td class="type">Type</td>
-						<td class="montant">Montant</td>
-						<td class="compte">Compte</td>
-						<td class="verifie">V&eacute;rifier</td>
-						<td></td>
-						<td></td>
-						<td></td>
-					</tr>';
+		if (isset($flag) and $flag==true) {
+			echo '<section class="panel col-100">
+					<section class="panel-header">
+						<span><i class="fa fa-money"></i>Résultat(s) de la recherche</span>
+					</section>
+					<section class="panel-body">
+						<section class="panel-body-header">
+							'.$html.'
+							Il y a <span>'.$nb_op.'</span> op&eacute;ration';if ($nb_op>1) echo 's'; echo ' bancaire';if ($nb_op>1) echo 's';
+							echo ' dans tous vos comptes pour votre recherche.
+						</section>
+							<table cellspacing="0" cellpadding="0" class="tList">
+							<tr class="tHeader">
+								<td></td>
+								<td class="nom">Nom de l\'op&eacute;ration bancaire</td>
+								<td class="date">Date</td>
+								<td class="type">Type</td>
+								<td class="montant">Montant</td>
+								<td class="compte">Compte</td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>';
 			$x=0;
 			while ($datas=$req->fetch(PDO::FETCH_ASSOC)) {
 				$x=2-$x;
@@ -132,7 +136,13 @@
 				else {
 					echo'<tr class="op_bancaire_not_verif '; if ($x==0) echo ' impair"'; echo '">';
 				}
-					echo '<td class="nom"><span title="#'.$datas['id'].'">'.charSpec2code($datas['nom'], "deconvert").'</span></td>';
+						echo '<td>';
+							if ($datas['verif']=='1')
+								echo '<img src="templates/images/verified.png" title="Op&eacute;ration bancaire v&eacute;rifi&eacute;e" />';
+							else
+								echo '<a href="?m=view_op&account='.$_GET['account'].'&verif='.$datas['id'].'"><img src="templates/images/unverified.png" title="V&eacute;rifer l\'op&eacute;ration" /></a>';
+						echo '</td>';
+						echo '<td class="nom"><span title="id:'.$datas['id'].'">'.charSpec2code($datas['nom'], "deconvert").'</span></td>';
 						echo '<td>'.build_date_fr($datas['op_date']).'</td>';
 						echo '<td>';
 							if ($datas['type']=='Cr&eacute;dit')
@@ -149,15 +159,13 @@
 							echo $datas['montant'].' €</span>';
 						echo '</td>';
 						echo '<td class="compte">'.$datas3['nom'].'</td>';
-						echo '<td class="verifie">';
-							if ($datas['verif']=='0') echo '<a href="?m=view_op&account='.$datas['id_compte'].'&verif='.$datas['id'].'">V&eacute;rifi&eacute;</a>';
-						echo '</td>';
 						echo '<td><div class="newcom"><a href="./" /><img src="templates/images/empty.png" title="Ajouter un commentaire" /></a></div></td>';
 						echo '<td><div class="edit"><a href="./" /><img src="templates/images/empty.png" title="Editer" /></a></div></td>';
 						echo '<td><div class="del"><a href="./" /><img src="templates/images/empty.png" title="Supprimer" /></a></div></td>';
 					echo '</tr>';
 			}
-			echo '</table>';
+			echo '</table>
+				</section>';
 		}
 	}
 	else {
